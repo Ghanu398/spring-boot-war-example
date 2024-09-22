c260fc0da2c789f0729eb6e20b8e50ac642243c9
Ghanu398 <96722604+Ghanu398@users.noreply.github.com>
2024-09-23 00:12:10 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Learning jenkins is fun";
+        return "Learning jenkins is fun 9-23";
     }
 
 }
